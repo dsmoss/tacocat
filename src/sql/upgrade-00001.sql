c2fcd3ed24d7cd05a8411e9e068f64d75001a4cb
dsm <dsmoss@gmail.com>
2019-08-29 23:00:54 -0500

    upgrade sql

--- src/sql/upgrade-00001.sql
+++ src/sql/upgrade-00001.sql
@@ -1,8 +1,20 @@
+-- we want to fail here if table exists
+create table app_upgrade
+  ( upgrade varchar(20) not null primary key
+  , date    timestamp   not null default now()
+);
+
+-- normally this would be the first line of every upgrade script
+insert into app_upgrade (upgrade)
+                 values ('upgrade-00001.sql')
+;
+
 alter table app_user add column enabled bool default true;
 
 insert into permission (name)
-values ('add-new-menu-group')
-     , ('change-user-enabled');
+                values ('add-new-menu-group')
+                     , ('change-user-enabled')
+;
 
 insert into role_permission (id_role, id_permission)
 select r.id
@@ -11,5 +23,6 @@ from   role       as r
 join   permission as p
   on   1 = 1
 where  r.name = 'Admin'
-  and  p.name in ('add-new-menu-group'
-                , 'change-user-enabled');
+  and  p.name in ( 'add-new-menu-group'
+                 , 'change-user-enabled'
+);
